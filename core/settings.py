e48df955c3098c8e9adf8adf816ff05cce32d7f1
Kassandra Ramos Vega <kramosv@inacap.cl>
2025-08-22 17:48:21 -0400

    prueba

--- core/settings.py
+++ core/settings.py
@@ -191,7 +191,7 @@ LOGGING = {
 
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = config("EMAIL_HOST", default="smtp.gmail.com")
-EMAIL_PORT = config("EMAIL_PORT", cast=int, default=465)
+EMAIL_PORT = config("EMAIL_PORT", cast=int, default=587)
 EMAIL_USE_TLS = config("EMAIL_USE_TLS", cast=bool, default=True)
 EMAIL_USE_SSL = config("EMAIL_USE_SSL", cast=bool, default=False)
 EMAIL_HOST_USER = config("EMAIL_HOST_USER")
